77b97dd522853051b78fe0aa559b42456e03e533
SadikRa <sadikrahman494@gmail.com>
2025-03-27 23:26:29 +0600

    create find done

---
+++ src/create.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  //   const result = await prisma.post.create({
+  //     data: {
+  //       title: "This is title",
+  //       content: " this is content",
+  //       author: "this is author",
+  //     },
+  //   });
+
+  //   console.log(result);
+  
+
+  //create many
+  const createMany = await prisma.post.createMany({
+    data: [
+      {
+        title: "title 1",
+        content: " content 1",
+        author: "i am muhammad sadik rahman",
+      },
+      {
+        title: "title 2",
+        content: " content 2",
+        author: "i am muhammad sadik rahman",
+      },
+      {
+        title: "title 3",
+        content: " content 3",
+        author: "i am muhammad sadik rahman",
+      },
+    ],
+  });
+  console.log(createMany);
+};
+
+main();
---
+++ src/find.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  // find all
+  const getAllFromDB = await prisma.post.findMany();
+
+  const findFirst = await prisma.post.findFirstOrThrow({
+    where: {
+      published: false,
+    },
+  });
+
+  //find unique and find first or throw
+  const findUnique = await prisma.post.findUniqueOrThrow({
+    where: {
+      id: 2,
+    },
+  });
+
+  console.log({ findUnique });
+};
+
+main();
