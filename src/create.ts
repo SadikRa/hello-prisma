8821c5a4f57360e3ec9724675f8dc13e2dfd7758
SadikRa <sadikrahman494@gmail.com>
2025-04-01 23:10:44 +0600

    update

--- src/create.ts
+++ src/create.ts
@@ -1,39 +1,63 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient, UserRole } from "../generated/prisma";
 
 const prisma = new PrismaClient();
 
 const main = async () => {
-  //   const result = await prisma.post.create({
-  //     data: {
-  //       title: "This is title",
-  //       content: " this is content",
-  //       author: "this is author",
-  //     },
-  //   });
+  // create user
 
-  //   console.log(result);
+  // const createUser = await prisma.user.create({
+  //   data: {
+  //     userName: "user1",
+  //     email: "user1@ph.com",
+  //     role: UserRole.user
 
-  //create many
-  const createMany = await prisma.post.createMany({
-    data: [
-      {
-        title: "title 1",
-        content: " content 1",
-        author: "i am muhammad sadik rahman",
+  //   }
+  // })
+
+  // console.log(createUser);
+
+  /// create profile
+
+  // const createProfile = await prisma.profile.create({
+  //   data: {
+  //     bio: "this is bio",
+  //     userId: 1
+  //   }
+  // })
+  // console.log(createProfile);
+
+  // create category
+
+  // const createCategory = await prisma.category.create({
+  //   data: {
+  //     name: "soft ware engineering"
+  //   }
+  // })
+  // console.log(createCategory);
+
+  // create post
+
+  const createPost = await prisma.post.create({
+    data: {
+      title: "this is title",
+      content: "this is content of the post",
+      authorId: 1,
+      postCategory: {
+        create: [
+          {
+            categoryId: 1,
+          },
+          {
+            categoryId: 6,
+          },
+        ],
       },
-      {
-        title: "title 2",
-        content: " content 2",
-        author: "i am muhammad sadik rahman",
-      },
-      {
-        title: "title 3",
-        content: " content 3",
-        author: "i am muhammad sadik rahman",
-      },
-    ],
+    },
+    include: {
+      postCategory: true,
+    },
   });
-  console.log(createMany);
+  console.log(createPost);
 };
 
 main();
---
+++ src/relational-queries.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient();
+
+const relationalQueries = async () => {
+  const result = await prisma.user.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      Post: true,
+    },
+  });
+
+  console.log(result);
+};
+
+relationalQueries();
