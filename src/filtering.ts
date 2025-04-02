fe5036ba4ee82c40ea8a742935eacf22028a02e5
SadikRa <sadikrahman494@gmail.com>
2025-04-02 20:40:03 +0600

    updated

--- src/filtering.ts
+++ src/filtering.ts
@@ -1,8 +1,35 @@
 import { PrismaClient } from "../generated/prisma";
 
-const prisma = new PrismaClient()
-
+const prisma = new PrismaClient();
 
 const filtering = async () => {
+  const andFiltering = await prisma.post.findMany({
+    where: {
+      AND: [
+        {
+          title: { contains: "title" },
+        },
+        {
+          published: true,
+        },
+      ],
+    },
+  });
 
-}
+  const orFiltering = await prisma.post.findMany({
+    where: {
+      OR: [
+        {
+          title: { contains: "title" },
+        },
+        {
+          published: true,
+        },
+      ],
+    },
+  });
+
+  console.log(orFiltering);
+};
+
+filtering();
