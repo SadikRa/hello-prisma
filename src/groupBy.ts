45aca281e38f5d3c0bec0187fd3ddbbc6f596ce8
SadikRa <sadikrahman494@gmail.com>
2025-04-05 21:08:35 +0600

    updated

---
+++ src/groupBy.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient();
+
+const groupBy = async () => {
+  const groupPost = await prisma.post.groupBy({
+    by: ["published", "authorId"],
+    _count: {
+      title: true,
+    },
+    having: {
+      authorId: {
+        _sum: {
+            gt: 1
+        }
+      }
+    },
+  });
+
+  console.log(groupPost);
+};
+
+groupBy();
