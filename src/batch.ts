76ee32744cf0e74a9bc0d3d030811d704124622e
SadikRa <sadikrahman494@gmail.com>
2025-04-05 22:54:50 +0600

    updated interacitve tra

--- src/batch.ts
+++ src/batch.ts
@@ -3,7 +3,29 @@ import { PrismaClient } from "../generated/prisma";
 const prisma = new PrismaClient();
 
 const batchTransaction = async () => {
-    
+  const createUser = prisma.user.create({
+    data: {
+      userName: "sadikRahmanTwin",
+      email: "sadikrahmanTwin@gmail.com",
+    },
+  });
+
+  const updateUser = prisma.user.update({
+    where: {
+      id: 3,
+    },
+    data: {
+      age: 30,
+    },
+  });
+
+  const [userData, updateData] = await prisma.$transaction([
+    createUser,
+    updateUser,
+  ]);
+
+  console.log("created user:", userData);
+  console.log("updated user:", updateData);
 };
 
 batchTransaction();
