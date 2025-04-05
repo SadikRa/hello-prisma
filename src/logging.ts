fecaa5be18c4d014b15463e5729ad2072e7e3ffe
SadikRa <sadikrahman494@gmail.com>
2025-04-05 11:04:19 +0600

    updated

--- src/logging.ts
+++ src/logging.ts
@@ -1,10 +1,23 @@
 import { PrismaClient } from "../generated/prisma";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+  ],
+});
 
-const main = async() => {
-    const getAllFromDB = await prisma.post.findMany()
-    console.log(getAllFromDB);
-}
+prisma.$on("query", (e) => {
+    console.log("duration", e.duration);
+    console.log("timestamp" ,e.timestamp);
+    console.log("Query" ,e.query);
+})
 
-main()
+const main = async () => {
+  const getAllFromDB = await prisma.post.findMany();
+//   console.log(getAllFromDB);
+};
+
+main();
